09b1fee29e699dbea17b912a78b06189f1bb981f
Goemon5 <119960019+Goemon5@users.noreply.github.com>
2024-07-07 10:06:52 +0900

    add _inputform

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
-import Users from './components/Users';
+import "./App.css";
+import Users from "./components/Users";
+import Form from "./components/form/Form.tsx";
 
 function App() {
   return (
     <div className="App">
-     <Users/>
+      <Users />
+      <Form />
     </div>
   );
 }
